77e089bf20774795571d9a3df29e6a33821cb9b0
Deyvi <deyvi.pr@gmail.com>
2020-11-15 16:17:50 -0500

    Next app initial stage

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import s from './Footer.module.css'
+const Footer = () => {
+    return (
+        <footer className={s.container}>
+            Este es el footer
+        </footer>
+    )
+}
+
+export default Footer
---
+++ components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={s.container}>
+            Este es el header
+        </header>
+    )
+}
+
+export default Header
